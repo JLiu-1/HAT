358aa3996fa88698069a2fead5ac10396d4c94f1
meso272 <liujinyang1201@126.com>
2023-07-08 01:22:16 -0700

    cesm

--- hat/data/__init__.py
+++ hat/data/__init__.py
@@ -7,5 +7,6 @@ from basicsr.utils import scandir
 # scan all the files that end with '_dataset.py' under the data folder
 data_folder = osp.dirname(osp.abspath(__file__))
 dataset_filenames = [osp.splitext(osp.basename(v))[0] for v in scandir(data_folder) if v.endswith('_dataset.py')]
+print(dataset_filenames)
 # import all the dataset modules
 _dataset_modules = [importlib.import_module(f'hat.data.{file_name}') for file_name in dataset_filenames]
