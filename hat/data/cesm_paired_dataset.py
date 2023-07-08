a4d52059a147557a9981d7f67b3717119b7712be
meso272 <liujinyang1201@126.com>
2023-07-08 01:34:47 -0700

    cesm

--- hat/data/cesm_paired_dataset.py
+++ hat/data/cesm_paired_dataset.py
@@ -69,6 +69,9 @@ class CesmPairedImageDataset(data.Dataset):
             gt_size = self.opt['gt_size']
             # random crop
             img_gt, img_lq = paired_random_crop(img_gt, img_lq, gt_size, scale, gt_path)#to customize
+
+            print(img_gt.shape)
+            print(img_lq.shape)
             # flip, rotation
             #img_gt, img_lq = augment([img_gt, img_lq], self.opt['use_hflip'], self.opt['use_rot'])
 
