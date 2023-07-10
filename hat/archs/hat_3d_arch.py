8d095b9658b7098af2b2a4569b6aa1e5c52efe61
meso272 <liujinyang1201@126.com>
2023-07-09 22:43:44 -0700

    3d

--- hat/archs/hat_3d_arch.py
+++ hat/archs/hat_3d_arch.py
@@ -177,7 +177,7 @@ class WindowAttention(nn.Module):
         attn = (q @ k.transpose(-2, -1))
 
         relative_position_bias = self.relative_position_bias_table[rpi.view(-1)].view(
-            self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1)  # Wh*Ww,Wh*Ww,nH
+            self.window_size[0] * self.window_size[1]* self.window_size[2], self.window_size[0] * self.window_size[1]* self.window_size[2], -1)  # Wh*Ww*Wd,Wh*Ww*Wd,nH
         relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous()  # nH, Wh*Ww, Wh*Ww
         attn = attn + relative_position_bias.unsqueeze(0)
 
