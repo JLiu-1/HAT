5ac7ffef3a605d29ecd87ba8bdc651a6e71a0506
meso272 <liujinyang1201@126.com>
2023-07-11 19:34:07 -0700

    3d

--- hat/data/2d_dataset.py
+++ hat/data/2d_dataset.py
@@ -81,8 +81,8 @@ class Sci2DDataset(data.Dataset):
             # random crop
             img_gt, img_lq = paired_random_crop(img_gt, img_lq, gt_size, scale, gt_path)#to customize
 
-            print(img_gt.shape)
-            print(img_lq.shape)
+            #print(img_gt.shape)
+            #print(img_lq.shape)
             #flip, rotation
             #img_gt, img_lq = augment([img_gt, img_lq], self.opt['use_hflip'], self.opt['use_rot'])#to customize
         '''
