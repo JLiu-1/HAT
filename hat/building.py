64a6ca739ddae3cf38f801a30e7f230be6d4ef48
meso272 <liujinyang1201@126.com>
2023-07-14 15:01:05 -0700

    3d

---
+++ hat/building.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import numpy as np 
+
+ifolder=sys.argv[1]
+ofile=sys.argv[2]
+size_x=eval(sys.argv[3])#hr
+size_y=eval(sys.argv[4])#hrhr
+size_z=eval(sys.argv[5])#hr
+
+res=np.zeros((size_x,size_y,size_z),dtype=np.float32)
+filelist=[f for f in os.listdir(ifolder) if ".dat" in f]
+
+for f in filelist:
+    ipath=os.path.join(ifolder,f)
+    arr=np.fromfile(ipath,dtype=np.float32)
+    spl=f.split("_")
+    xyz=spl[0]
+    idx=eval(spl[1])
+    if xyz=="x":
+        arr=arr.reshape((size_y,size_z))
+        res[idx,:,:]=arr
+    elif xyz=="y":
+        arr=arr.reshape((size_x,size_z))
+        res[:,idx,:]=arr
+    elif xyz=="z":
+        arr=arr.reshape((size_x,size_y))
+        res[:,:,idx]=arr
+
+res.tofile(ofile)
---
+++ hat/slicing.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import numpy as np 
+
+ifile=sys.argv[1]
+ofolder=sys.argv[2]
+size_x=eval(sys.argv[3])#lr
+size_y=eval(sys.argv[4])#lr
+size_z=eval(sys.argv[5])#lr
+scale=eval(sys.argv[6])
+arr=np.fromfile(ifile,dtype).reshape((size_x,size_y,size_z))
+
+for x in range(size_x):
+    ofile="x_%d.dat" %(x*scale)
+    opath=os.path.join(ofolder,opath)
+
+    arr[x,:,:].tofile(opath)
+
+for y in range(size_y):
+    ofile="y_%d.dat" %(y*scale)
+    opath=os.path.join(ofolder,opath)
+
+    arr[:,y,:].tofile(opath)
+
+for z in range(size_z):
+    ofile="z_%d.dat" %(z*scale)
+    opath=os.path.join(ofolder,opath)
+
+    arr[:,:,z].tofile(opath)
+
+
