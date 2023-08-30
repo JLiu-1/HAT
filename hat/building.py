f4e34937cf977dd9199c8a3f55b78a26b94074df
meso272 <liujinyang1201@126.com>
2023-08-30 00:13:49 -0700

    3dt

--- hat/building.py
+++ hat/building.py
@@ -9,6 +9,7 @@ size_y=eval(sys.argv[4])#hrhr
 size_z=eval(sys.argv[5])#hr
 
 res=np.zeros((size_x,size_y,size_z),dtype=np.float32)
+counts=np.zeros((size_x,size_y,size_z),dtype=np.float32)
 filelist=[f for f in os.listdir(ifolder) if ".dat" in f]
 
 for f in filelist:
@@ -17,6 +18,7 @@ for f in filelist:
     spl=f.split("_")
     xyz=spl[0]
     idx=eval(spl[1])
+    '''
     if xyz=="x":
         arr=arr.reshape((size_y,size_z))
         res[idx,:,:]=arr
@@ -26,5 +28,23 @@ for f in filelist:
     elif xyz=="z":
         arr=arr.reshape((size_x,size_y))
         res[:,:,idx]=arr
+    '''
+
+    if xyz=="x":
+        arr=arr.reshape((size_y,size_z))
+        for i in range(size_y):
+            for j in range(size_z):
+
+        res[idx,:,:]+=arr
+        counts[idx,:,:]+=np.ones((size_y,size_z),dtype=np.float32)
+    elif xyz=="y":
+        arr=arr.reshape((size_x,size_z))
+        res[:,idx,:]+=arr
+        counts[:,idx,:]+=np.ones((size_x,size_z),dtype=np.float32)
+    elif xyz=="z":
+        arr=arr.reshape((size_x,size_y))
+        res[:,:,idx]+=arr
+        counts[:,:,idx]+=np.ones((size_x,size_y),dtype=np.float32)
+res=np.divide(res,counts)
 
 res.tofile(ofile)
